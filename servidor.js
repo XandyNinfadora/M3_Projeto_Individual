d22abe3c519b7b1fbc994563c9a075a55bcd55af
XandyNinfadora <xandriellepessanhax@gmail.com>
2022-11-11 18:58:50 -0300

    testes finais

--- servidor.js
+++ servidor.js
@@ -1,10 +1,12 @@
-const jsonServer = require('json-server')
-const servidor = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const servidor = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+servidor.use(middlewares);
+servidor.use(router);
 const port = process.env.PORT || 3000;
 
-servidor.use(middlewares)
-servidor.use(router)
-
-servidor.listen(port)
+servidor.listen(port, ()=> {
+    console.log(`Pegou Bro ${port}`)
+});
