d8586a93a88cd6f2b98abeb8b5b6c60bccfccf12
XandyNinfadora <xandriellepessanhax@gmail.com>
2022-11-11 18:25:30 -0300

    atualizando

--- servidor.js
+++ servidor.js
@@ -1,11 +1,10 @@
-const express = require ('express');
-const app = express();
-const jsonServer = require ('json-server');
-const servidor = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8081;
+const jsonServer = require('json-server')
+const servidor = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+const port = process.env.PORT || 3000;
 
-servidor.use (middlewares);
-servidor.use (router);
-servidor.listen(port);
+servidor.use(middlewares)
+servidor.use(router)
+
+servidor.listen(port)
